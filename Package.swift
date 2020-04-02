1da95b17f235c6029a0d307fcc0a1abfcdd6aa4a
Sam Deane <sam@elegantchaos.com>
2020-04-02 10:54:15 +0100

    updated bundle utilities

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["ApplicationExtensions"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/elegantchaos/Bundles.git", from: "1.0.0"),
+        .package(url: "https://github.com/elegantchaos/Bundles.git", from: "1.0.1"),
         .package(url: "https://github.com/elegantchaos/Logger.git", from: "1.5.3"),
         .package(url: "https://github.com/elegantchaos/CollectionExtensions.git", from: "1.0.1"),
     ],
